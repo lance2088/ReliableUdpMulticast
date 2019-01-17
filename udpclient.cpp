a3c65185a5c16d9f0a41b617d5dc46d10cc5c6d8
Krzysztof Michalak <krzysztof.michalak96@gmail.com>
2019-01-17 20:05:03 +0100

    Correct mistakes

--- udpclient.cpp
+++ udpclient.cpp
@@ -21,23 +21,13 @@ UdpClient::UdpClient(int domain, int type, int protocol, const char *client_ip, 
         perror ("Cannot create the socket");
         exit (EXIT_FAILURE);
     }
-    else
-    {
-        printf("Socket created\n");
-        printf("sock: %d\n", sock);
-        printf("this->sock: %d\n", this->sock);
-    }
 
-
-    // own address structure
     this->myaddr = generate_address_structure(client_ip, port);
-
     bind(sock, (struct sockaddr*)&myaddr, sizeof(struct sockaddr));
 }
 
 void UdpClient::sendToIp(const char *ip_address, const char *port, char *buf)
 {
-    // server address structure
     struct sockaddr_in serv_addr = generate_address_structure(ip_address, port);
 
     socklen_t serv_addr_size = sizeof(struct sockaddr);
@@ -45,17 +35,15 @@ void UdpClient::sendToIp(const char *ip_address, const char *port, char *buf)
     if(sendto(sock, buf, strlen(buf), 0, (struct sockaddr*)&serv_addr, serv_addr_size) < 0 )
         {
             perror( "sendto() ERROR" );
-            exit( 1 );
+            exit (EXIT_FAILURE);
         }
 }
 
 void UdpClient::recvFromIp(const char *ip_address, const char *port, char *buf)
 {
-    // server address structure
     struct sockaddr_in serv_addr = generate_address_structure(ip_address, port);
 
     socklen_t serv_addr_size = sizeof(struct sockaddr);
-    // not sure if there should be strlen(buf+1) or strlen(buf)
     recvfrom(sock, buf, strlen(buf), 0, (struct sockaddr*)&serv_addr, &serv_addr_size);
 
 }
